00be2352d894bc554a694b214d93e50a493ae207
Jena Reis <jenadreis@gmail.com>
2022-01-15 12:28:54 -0500

    set up check answer

---
+++ script.js
@@ -0,0 +1,31 @@
+const answer = 'tangy';
+
+const submitBtn = document.querySelector('#submit');
+submit.maxLength = answer.length
+
+const checkAnswer = (guess) => {
+    console.clear()
+    if (guess.length !== answer.length) {
+        console.log('Not long enough');
+    } else if (guess === answer) {
+        console.log('You Win!');
+    } else {
+        for (let i = 0; i < guess.length; i++) {
+            const g = guess[i];
+            const a = answer[i];
+            if (g === a) {  // correct letter and correct spot
+                console.log(`${g} is in the right spot`);
+            } else if (answer.includes(g)) { // correct letter and wrong spot
+                console.log(`${g} is in the wrong spot`);
+            } else { // incorrect letter for word
+                console.log(`${g} is not correct`);
+            }
+        }
+    }
+};
+
+submitBtn.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter') {
+        checkAnswer(submit.value)
+    }
+});
